df1e6678146f5628e4ac51859de3c62a276344cf
Kivanc Tatar <2469227-ktatar@users.noreply.gitlab.com>
2020-02-09 15:51:26 +0100

    cloud runs

--- utils/jb-generate-examples.sh
+++ utils/jb-generate-examples.sh
@@ -16,4 +16,4 @@ source /home/$USER/tf2_gpu/bin/activate
 sleep 5
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EBROOTCUDA/lib64:$EBROOTCUDNN/lib64
 
-python generate_interpolations.py --dir /home/ktatar/scratch/datasets/ambient-lawrence-cruel/func-timbre-cvae-leaky-cedar/run-000
+python generate_interpolations.py --dir  /home/ktatar/scratch/datasets/Electroacoustic-Roads-with-silence/func-timbre-cvae-cedar/run-004
