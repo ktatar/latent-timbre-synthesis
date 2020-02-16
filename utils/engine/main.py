3ab57f403ffae1873f110595f7a5b0be37dfbddc
Kivanc Tatar <2469227-ktatar@users.noreply.gitlab.com>
2020-02-16 03:55:19 +0100

    working on the user interface for the interpolation

--- utils/engine/main.py
+++ utils/engine/main.py
@@ -3,18 +3,21 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 from tensorflow.keras import layers
+tf.keras.backend.clear_session()  # For easy reset of notebook state.
 
 import random
 import numpy as np
 import ctypes
 
 import os, sys, argparse, time, random
+from pathlib import Path
 import librosa
 import pdb
 import configparser
 from pythonosc import dispatcher
 from pythonosc import osc_server
 from typing import List, Any
+from pythonosc.udp_client import SimpleUDPClient
 
 import warnings 
 def custom_formatwarning(msg, *args, **kwargs):
@@ -25,12 +28,41 @@ warnings.formatwarning = custom_formatwarning
 
 #tensorflow autotune
 AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+#Global Variables
 #verbose for the extra prints
 verbose = True
+normalize = False
 config = {}
 load_datasetpath_with_model = False
 dataset = ''
 run_path = ''
+my_audio = ''
+my_cqt = ''
+n_iter = 1
+sound = {
+  'interpolate_two':{
+    'first':{
+      'name': '',
+      'audio_path': '',
+      'duration': 0,
+      'cqt_path':'',
+      'CQT': np.empty([0])
+    },
+    'second':{
+      'name': '',
+      'audio_path': '',
+      'duration': 0,
+      'cqt_path':'',
+      'CQT': np.empty([0])
+    } 
+  }
+}
+
+trained_model = tf.keras.Model()
+encoder = tf.keras.Model()
+decoder = tf.keras.Model()
+
 #We need to define the custom Sampling Layer
 class Sampling(layers.Layer):
   """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
@@ -46,45 +78,66 @@ def verbose(new_verbose):
   global verbose
   verbose = new_verbose
 
-def initiate_dataset(address: str, *osc_arguments: List[Any]) -> None:
-  if os.path.exists(osc_arguments[0]): 
-    dataset = osc_arguments[0]
-    print('Dataset is set to: \n {}'.format(osc_arguments[0]))
-  else:
-    warnings.warn('Could not set the dataset. The path does not exist. Received the following OSC message: \n {}'.format(osc_arguments[:]))
-def load_model(address: str, *osc_arguments: List[Any]) -> None:
-  global dataset, run_path, my_cqt, my_audio
-  run_path = osc_arguments[0]
+def set_load_datasetpath_with_model(my_state=False):
+  global load_datasetpath_with_model
 
+  #if you want to set the dataset path directly from the config file, 
+  load_datasetpath_with_model = my_state
+
+def warning_plus_osc(my_warning):
+  warnings.warn(my_warning)
+  client.send_message("/report", my_warning)
+
+def print_plus_osc(my_report):
+  print(my_report)
+  client.send_message("/report", my_report)
+
+def initiate_dataset(address: str, *osc_arguments: List[Any]) -> None:
+  global dataset
+
+  if os.path.exists(Path(osc_arguments[0])): 
+    dataset = Path(osc_arguments[0])
+    my_report = 'Dataset is set to: \n {}'.format(dataset.resolve())
+    print(my_report)
+    client.send_message("/dataset", str(dataset.resolve()) )   # Send float message
+    client.send_message("/report", my_report)
+  else:
+    my_warning = 'Could not set the dataset. The path does not exist. Received the following OSC message: \n {}'.format(osc_arguments[:])
+    warning_plus_osc(my_warning)
+
+def load_model(address: str, *osc_arguments: List[Any]) -> None:
+  global dataset, run_path, my_cqt, my_audio, trained_model, encoder, decoder
+  run_path = Path(osc_arguments[0])
+
+  #reports
+  print_plus_osc('Current dataset path is {}'.format( str(dataset.resolve())))
+  print_plus_osc('Received a request to load model from path {}'.format( str(dataset.resolve())))
+  
   if not os.path.exists(run_path):
-    raise FileNotFoundError('Run path does not exist: \n {} \n Could not load the DL model'.format(run_path))
+    my_warning = 'Run path does not exist: \n {} \n Could not load the DL model'/format(str(run_path.resolve()))
+    warning_plus_osc(my_warning)
+    return
 
   config = configparser.ConfigParser(allow_no_value=True)
-  config_path = os.path.join(run_path, 'config.ini')
+  config_path = run_path / 'config.ini'
   config.read(config_path)
 
   cqt_dataset = config['dataset'].get('cqt_dataset')
-  my_cqt = os.path.join(dataset, cqt_dataset)
-  print('CQT dataset set to \n {}'.format(my_cqt))
-  my_audio = os.path.join(dataset, 'audio')
-  print('Audio path set to \n {}'.format(my_audio))
+  my_cqt = Path(dataset) / cqt_dataset
+  print_plus_osc('CQT dataset set to \n {}'.format(my_cqt))
+  my_audio = Path(dataset) / 'audio'
+  print_plus_osc('Audio path set to \n {}'.format(my_audio))
 
   #dataset
   if load_datasetpath_with_model:
     dataset = config['dataset'].get('datapath')
-    print('Reverting back to the training dataset path \n {}'.format(dataset))
-
-  cqt_dataset = config['dataset'].get('cqt_dataset')
-  my_cqt = os.path.join(config['dataset'].get('datapath'), cqt_dataset)
-  print('CQT dataset set to \n {}'.format(my_cqt))
-  my_audio = os.path.join(config['dataset'].get('datapath'), 'audio')
-  print('Audio path set to \n {}'.format(my_audio))
+    print_plus_osc('Reverting back to the training dataset path \n {}'.format(str(dataset.resolve())))
 
   #Training configs
   batch_size = config['training'].getint('batch_size')
-
+  client.send_message("/model/batch_size", batch_size) 
   #load the model
-  my_model_path = os.path.join(os.path.join(run_path, 'model'),'mymodel_last.h5')
+  my_model_path = run_path / 'model' / 'mymodel_last.h5'
 
   with tf.keras.utils.CustomObjectScope({'Sampling': Sampling}):
     trained_model = tf.keras.models.load_model(my_model_path)
@@ -104,16 +157,142 @@ def load_model(address: str, *osc_arguments: List[Any]) -> None:
   if verbose: 
     decoder.summary()
 
-  print('Model loaded successfully from \n {}'.format(run_path))
+  print_plus_osc('Model loaded successfully from \n {}'.format(run_path))
+
+def set_i2_first_sound(address: str, *osc_arguments: List[Any]) -> None:
+  global sound
   
+  audio_path = my_audio / (osc_arguments[0]+'.wav')
+  cqt_path = my_cqt / (osc_arguments[0]+'.npy')
+
+  if os.path.exists(audio_path) and os.path.exists(cqt_path): 
+    sound['interpolate_two']['first']['name'] = osc_arguments[0]
+    sound['interpolate_two']['first']['audio_path'] = str(audio_path.resolve())
+    sound['interpolate_two']['first']['cqt_path'] = str(cqt_path.resolve())
+    my_report = 'First sound is set to: \n {} \n at the path \n {}'.format( sound['interpolate_two']['first']['name'], sound['interpolate_two']['first']['audio_path'], sound['interpolate_two']['first']['cqt_path'])
+    
+    print_plus_osc(my_report)
+    client.send_message("/sound/interpolate_two/first/path",  sound['interpolate_two']['first']['audio_path'])
+    client.send_message("/sound/interpolate_two/first/name",  sound['interpolate_two']['first']['name'])
+
+    #Load Audio 1 
+    sound['interpolate_two']['first']['duration'] = librosa.get_duration(filename=audio_path)
+    sound['interpolate_two']['first']['CQT'] = np.load(os.path.join(my_cqt, sound['interpolate_two']['first']['name']+'.npy'))
+
+  else:
+    my_warning = 'Could not set the first sound for interpolate_two. Either audio or CQT path do not exist. Received the following OSC message: \n {} \n generated the following audio path \n {} \n generated following CQT path \n {}'.format(osc_arguments[0], audio_path, cqt_path)
+    warning_plus_osc(my_warning)
+
+def set_i2_second_sound(address: str, *osc_arguments: List[Any]) -> None:
+  global sound
+  
+  audio_path = my_audio / (osc_arguments[0]+'.wav')
+  cqt_path = my_cqt / (osc_arguments[0]+'.npy')
+  
+  if os.path.exists(audio_path) and os.path.exists(cqt_path): 
+    sound['interpolate_two']['second']['name'] = osc_arguments[0]
+    sound['interpolate_two']['second']['audio_path'] = str(audio_path.resolve())
+    sound['interpolate_two']['second']['cqt_path'] = str(cqt_path.resolve())
+    my_report = 'Second sound is set to: \n {} \n at the path \n {}'.format(sound['interpolate_two']['second']['name'],sound['interpolate_two']['second']['audio_path'], sound['interpolate_two']['second']['cqt_path']  )
+    
+    print_plus_osc(my_report)
+    
+    client.send_message("/sound/interpolate_two/second/path", sound['interpolate_two']['second']['audio_path'])
+    client.send_message("/sound/interpolate_two/second/name", sound['interpolate_two']['second']['name'])   
+
+    #Load Audio 1 
+    sound['interpolate_two']['first']['duration'] = librosa.get_duration(filename=audio_path)
+    sound['interpolate_two']['first']['CQT'] = np.load(os.path.join(my_cqt, sound['interpolate_two']['first']['name']+'.npy'))
+
+  #If path does not exists  
+  else:
+    my_warning = 'Could not set the first sound. The path does not exist. Received the following OSC message: \n {} \n generated the following path \n Audio:{} \n CQT:{}'.format(osc_arguments[0], audio_path, cqt_path)
+    warning_plus_osc(my_warning)
+
+def set_batch_size(address: str, *osc_arguments: List[Any]) -> None:
+
+  global batch_size
+
+  batch_size = int(osc_arguments[0])
+  client.send_message("/model/batch_size", batch_size) 
+
+  print_plus_osc('Batch size is set to: {}'.format(osc_arguments[0]))
+
+def set_griffin_lim_iterations(address: str, *osc_arguments: List[Any]) -> None:
+  #Expects an integer to set number of iterations for the griffin lim algorithm
+
+  global n_iter
+
+  n_iter = int(osc_arguments[0])
+  client.send_message("/output/gf_iter", n_iter) 
+
+  print_plus_osc('Griffin Lim number of iterations is set to: {}'.format(osc_arguments[0]))
+
+def set_normalization(address: str, *osc_arguments: List[Any]) -> None:
+  global normalize
+  batch_size = int(osc_arguments[0])
+  client.send_message("/output/normalize", normalize) 
+  if normalize:
+    print_plus_osc('Output normalization is on.')
+  else:
+    print_plus_osc('Output normalization is off.')
+
+def generate_sound(address: str, *osc_arguments: List[Any]) -> None:  
+  if len(osc_arguments) != 3:
+    warning_plus_osc('Generate sound function did not receive enough arguments ({}). Required number of arguments are 3: duration, audio_1_offset, audio_2_offset'.format(len(osc_arguments)))
+    return
+  #import audio configs 
+  sample_rate = config['audio'].getint('sample_rate')
+  hop_length = config['audio'].getint('hop_length')
+  bins_per_octave = config['audio'].getint('bins_per_octave')
+  num_octaves = config['audio'].getint('num_octaves')
+  n_bins = int(num_octaves * bins_per_octave)
+  n_iter = config['audio'].getint('n_iter')
+  #Model configs
+  if 'VAE' in config:
+    config['model'] = config['VAE'] 
+  elif 'CVAE' in config:
+    config['model'] = config['CVAE']
+  else:
+    warning_plus_osc('Could not find the model parameters in the config. Tried key options CVAE and VAE.')
+    return  
+
+  latent_dim = config['model'].getint('latent_dim')
+
+  example_length = int(osc_arguments[0])
+  if verbose:
+    print_plus_osc('Interpolate 2 - duration set to {}'.format(example_length))
+  
+
+  interpolations_audio_1 = sound['interpolate_two']['first']['name']
+  audio_1_offset = osc_arguments[1]
+  
+  interpolations_audio_2 = sound['interpolate_two']['second']['name']
+  audio_2_offset = osc_arguments[2]
+  
+  if verbose:
+    print_plus_osc('First audio: {} \n First audio offset: {} \n Second audio: {} \n Second audio offset: {}'.format(interpolations_audio_1, audio_1_offset, interpolations_audio_2, audio_2_offset))
+  
+  #required for the initial test
+  os.makedirs(os.path.join(dataset, test),exist_ok=True)
+
+
 
 if __name__ == "__main__":
     #Parse arguments
   parser = argparse.ArgumentParser()
-  parser.add_argument("--ip", default="127.0.0.1",
+  parser.add_argument("--receiveIP", default="127.0.0.1",
         help="The ip of the OSC server")
-  parser.add_argument("--port", type=int, default=7010,
-        help="The port the OSC server is listening on")
+
+  parser.add_argument("--receivePORT", type=int, default=7020,
+        help="The port the OSC server is sending")
+  
+  parser.add_argument("--sendIP", default="127.0.0.1",
+        help="The ip of the OSC client")
+  
+  parser.add_argument("--sendPORT", type=int, default=7021,
+        help="The port the OSC client is sending")
+  
   parser.add_argument("--verbose", default=True,
       help="Should I report what is happening?")
   args = parser.parse_args()
@@ -129,9 +308,19 @@ if __name__ == "__main__":
   dispatcher = dispatcher.Dispatcher()
   dispatcher.map("/load/dataset", initiate_dataset)
   dispatcher.map("/load/model", load_model)
+  dispatcher.map("/generate", generate_sound)
+
+  #set params
+  dispatcher.map("/batch_size/set", set_batch_size)
+  dispatcher.map('/output/normalize', set_normalization)
+  dispatcher.map("/sound/interpolate_two/first/name", set_i2_first_sound)
+  dispatcher.map("/sound/interpolate_two/second/name", set_i2_second_sound)
+  dispatcher.map("/output/gf_iter", set_griffin_lim_iterations)
 
   server = osc_server.ThreadingOSCUDPServer(
-      (args.ip, args.port), dispatcher)
-  print("Serving on {}".format(server.server_address))
+      (args.receiveIP, args.receivePORT), dispatcher)
+  print("Listening to {}".format(server.server_address))
+  client = SimpleUDPClient(args.sendIP, args.sendPORT)  # Create client
+  print("Sending reports to {} {}".format(args.sendIP, args.sendPORT))
   server.serve_forever()
   
--- utils/engine/max_ui.maxpat
+++ utils/engine/max_ui.maxpat
@@ -10,7 +10,7 @@
 		}
 ,
 		"classnamespace" : "box",
-		"rect" : [ 58.0, 89.0, 1283.0, 480.0 ],
+		"rect" : [ 35.0, 85.0, 847.0, 400.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
@@ -38,6 +38,139 @@
 		"style" : "",
 		"subpatcher_template" : "",
 		"boxes" : [ 			{
+				"box" : 				{
+					"id" : "obj-30",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 287.0, 461.5, 77.0, 22.0 ],
+					"text" : "absolutepath"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-28",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 474.0, 473.0, 32.0, 22.0 ],
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-19",
+					"linecount" : 3,
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 870.0, 365.0, 321.0, 50.0 ],
+					"text" : "/load/model D:/my_workspace/dataset/Electroacoustic-Roads-with-silence/func-timbre-vae-cedar/run-012/"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-26",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 434.0, 549.0, 243.0, 22.0 ],
+					"text" : "/sound/interpolate_two/*/name \"05 Sculptor\""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-24",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 578.0, 449.0, 73.0, 22.0 ],
+					"text" : "print reports"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-23",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 152.0, 326.0, 59.0, 22.0 ],
+					"text" : "/generate"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-21",
+					"linecount" : 15,
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 755.0, 365.0, 50.0, 215.0 ],
+					"text" : "/load/model D:/my_workspace/dataset/Electroacoustic-Roads-with-silence/func-timbre-vae-cedar/run-012/"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-18",
+					"linecount" : 5,
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 9.0, 562.0, 389.0, 77.0 ],
+					"text" : "\"Second sound is set to: \n 05 Sculptor \n at the path \n D:my_workspacedatasetElectroacoustic-Roads-with-silenceaudio05 Sculptor.wav\""
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-17",
+					"maxclass" : "newobj",
+					"numinlets" : 3,
+					"numoutlets" : 3,
+					"outlettype" : [ "", "", "" ],
+					"patching_rect" : [ 287.0, 427.0, 120.0, 22.0 ],
+					"text" : "route /dataset /report"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-12",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 14.0, 517.0, 389.0, 22.0 ],
+					"text" : "D:/my_workspace/dataset/Electroacoustic-Roads-with-silence"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-9",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 287.0, 356.0, 97.0, 22.0 ],
+					"text" : "udpreceive 7021"
+				}
+
+			}
+, 			{
 				"box" : 				{
 					"id" : "obj-8",
 					"maxclass" : "newobj",
@@ -80,7 +213,7 @@
 					"maxclass" : "newobj",
 					"numinlets" : 1,
 					"numoutlets" : 0,
-					"patching_rect" : [ 239.0, 376.0, 138.0, 22.0 ],
+					"patching_rect" : [ 249.0, 304.0, 138.0, 22.0 ],
 					"text" : "udpsend 127.0.0.1 7020"
 				}
 
@@ -88,13 +221,13 @@
 , 			{
 				"box" : 				{
 					"id" : "obj-31",
-					"linecount" : 2,
+					"linecount" : 3,
 					"maxclass" : "message",
 					"numinlets" : 2,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 374.0, 421.0, 317.0, 36.0 ],
-					"text" : "/load/dataset /workspace/dataset/Electroacoustic-Gigantic/"
+					"patching_rect" : [ 384.0, 349.0, 317.0, 50.0 ],
+					"text" : "/load/dataset D:/my_workspace/dataset/Electroacoustic-Roads-with-silence/"
 				}
 
 			}
@@ -105,7 +238,7 @@
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 431.0, 387.0, 97.0, 22.0 ],
+					"patching_rect" : [ 441.0, 315.0, 97.0, 22.0 ],
 					"text" : "udpreceive 7020"
 				}
 
@@ -264,8 +397,8 @@
 					"maxclass" : "newobj",
 					"numinlets" : 1,
 					"numoutlets" : 0,
-					"patching_rect" : [ 93.0, 486.0, 138.0, 22.0 ],
-					"text" : "udpsend 127.0.0.1 7010"
+					"patching_rect" : [ 93.0, 392.0, 138.0, 22.0 ],
+					"text" : "udpsend 127.0.0.1 7020"
 				}
 
 			}
@@ -295,6 +428,15 @@
 , 			{
 				"patchline" : 				{
 					"destination" : [ "obj-6", 0 ],
+					"order" : 1,
+					"source" : [ "obj-15", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-7", 0 ],
+					"order" : 0,
 					"source" : [ "obj-15", 0 ]
 				}
 
@@ -315,6 +457,36 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-18", 1 ],
+					"order" : 1,
+					"source" : [ "obj-17", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-24", 0 ],
+					"order" : 0,
+					"source" : [ "obj-17", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-30", 0 ],
+					"source" : [ "obj-17", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-7", 0 ],
+					"source" : [ "obj-19", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-7", 0 ],
 					"source" : [ "obj-2", 0 ]
 				}
@@ -329,6 +501,20 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-21", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-23", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-4", 0 ],
 					"source" : [ "obj-25", 0 ]
 				}
@@ -336,8 +522,22 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-26", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-31", 1 ],
 					"source" : [ "obj-29", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-12", 1 ],
+					"source" : [ "obj-30", 0 ]
 				}
 
 			}
@@ -380,8 +580,33 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-1", 0 ],
+					"destination" : [ "obj-19", 1 ],
+					"order" : 0,
 					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-21", 1 ],
+					"order" : 1,
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-17", 0 ],
+					"order" : 1,
+					"source" : [ "obj-9", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-28", 0 ],
+					"order" : 0,
+					"source" : [ "obj-9", 0 ]
 				}
 
 			}
